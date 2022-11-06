239241575520b6dd5f610dd588cc9705404577c1
Jvson03 <jvsonlamaj@gmail.com>
2022-11-06 14:36:54 +0100

    empty spacings

--- assets/js/anim.js
+++ assets/js/anim.js
@@ -2,11 +2,9 @@ $(document).ready(function() {
 
     // Check for click events on the navbar burger icon
     $('.navbar-burger').click(function() {
-
         // Toggle the 'is-active' class on both the 'navbar-burger' and the 'navbar-menu'
         $('.navbar-burger').toggleClass('is-active');
         $('.navbar-menu').toggleClass('is-active');
-
     });
 });
 
@@ -31,7 +29,6 @@ function closeAllModals() {
     const modal = $trigger.dataset.target;
     const $target = document.getElementById(modal);
     console.log($target);
-
     $trigger.addEventListener('click', () => {
     openModal($target);
     });
@@ -40,7 +37,6 @@ function closeAllModals() {
 // Add a click event on various child elements to close the parent modal
 (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
     const $target = $close.closest('.modal');
-
     $close.addEventListener('click', () => {
     closeModal($target);
     });
@@ -49,7 +45,6 @@ function closeAllModals() {
 // Add a keyboard event to close all modals
 document.addEventListener('keydown', (event) => {
     const e = event || window.event;
-
     if (e.keyCode === 27) { // Escape key
     closeAllModals();
     }
