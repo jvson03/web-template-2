febe207f695fcf0542f2c804819947712b33c56c
Jvson03 <jvsonlamaj@gmail.com>
2022-11-06 14:35:32 +0100

    done

---
+++ assets/js/anim.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+
+    // Check for click events on the navbar burger icon
+    $('.navbar-burger').click(function() {
+
+        // Toggle the 'is-active' class on both the 'navbar-burger' and the 'navbar-menu'
+        $('.navbar-burger').toggleClass('is-active');
+        $('.navbar-menu').toggleClass('is-active');
+
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+// Functions to open and close a modal
+function openModal($el) {
+    $el.classList.add('is-active');
+}
+
+function closeModal($el) {
+    $el.classList.remove('is-active');
+}
+
+function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+    closeModal($modal);
+    });
+}
+
+// Add a click event on buttons to open a specific modal
+(document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+    console.log($target);
+
+    $trigger.addEventListener('click', () => {
+    openModal($target);
+    });
+});
+
+// Add a click event on various child elements to close the parent modal
+(document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+    const $target = $close.closest('.modal');
+
+    $close.addEventListener('click', () => {
+    closeModal($target);
+    });
+});
+
+// Add a keyboard event to close all modals
+document.addEventListener('keydown', (event) => {
+    const e = event || window.event;
+
+    if (e.keyCode === 27) { // Escape key
+    closeAllModals();
+    }
+});
+});
+
+$(window).on('load', function() {
+    $('.load').fadeOut();
+    console.log('Loaded');
+})
